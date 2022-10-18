e73f57e3244a1ee201b377edacc03a9668c6144a
Delanoor Nahm <hwn0809@gmail.com>
2022-10-18 15:48:52 +0900

    update

--- components/Slider.jsx
+++ components/Slider.jsx
@@ -26,17 +26,17 @@ function Slider() {
   const [activeAlbum, setActiveAlbum] = useState(albums[0].title);
 
   return (
-    <section className="section flex flex-col h-screen w-full">
-      <h1>Some Shit</h1>
-      <div className="testinonial-container w-[1280px] h-[680px] relative">
-        <div className="arrows-left">
+    <section className="section flex flex-col h-screen w-full justify-center items-center">
+      <h1 className="text-3xl font-bold my-3">Discography</h1>
+      <div className="testinonial-container w-[1280px] h-[680px] relative border ">
+        <div className="arrows-left absolute top-0 bottom-0 flex justify-center items-center cursor-pointer">
           <svg className="w-8 h-8">
             <ArrowLeftIcon />
           </svg>
         </div>
         <div className="inner flex justify-center items-center h-[600px]">
-          <div className="t-image w-[500px] h-[600px]">
-            <ul>
+          <div className="t-image w-[500px] h-[600px] after:content-[''] after:absolute after:w-[200px] after:h-[200px] after:background-[#3f56da] after:left-[140px] after:top-0 after:rounded-full">
+            <ul className="flex absolute border h-[460px] w-[460px]">
               {albums &&
                 albums.map((item, index) => {
                   return (
@@ -44,7 +44,7 @@ function Slider() {
                       key={index}
                       className={`${
                         activeAlbum === item.title ? "display" : "hidden"
-                      }`}
+                      } h-full w-full`}
                     >
                       <Image
                         src={item.image}
@@ -72,14 +72,14 @@ function Slider() {
                   >
                     <div className="content-inner">
                       <p className="quote">{item.title}</p>
-                      <p className="quote">{item.name}</p>
+                      <p className="quote font-bold">{item.name}</p>
                     </div>
                   </li>
                 );
               })}
           </ul>
         </div>
-        <div className="arrows-right">
+        <div className="arrows-right absolute top-0 bottom-0 flex justify-center items-center right-0 cursor-pointer">
           <svg className="w-8 h-8">
             <ArrowRightIcon />
           </svg>
