9af4d39279c867802bbf06b02fda458247c6dc10
Delanoor Nahm <hwn0809@gmail.com>
2022-10-20 14:34:28 +0900

    update styles

--- components/Slider.jsx
+++ components/Slider.jsx
@@ -28,15 +28,15 @@ function Slider() {
   return (
     <section className="section flex flex-col h-screen w-full justify-center items-center">
       <h1 className="text-3xl font-bold my-3">Discography</h1>
-      <div className="testinonial-container w-[1280px] h-[680px] relative border ">
-        <div className="arrows-left absolute top-0 bottom-0 flex justify-center items-center cursor-pointer">
+      <div className="testinonial-container w-[1280px] h-[680px] relative">
+        <div className="arrows-left absolute top-0 bottom-0 flex justify-center items-center cursor-pointer hover:shadow-[0px_0px_30px_rgba(0,0,80,0.05)] rounded-[8px] w-[100px] duration-300 ease-in-out">
           <svg className="w-8 h-8">
             <ArrowLeftIcon />
           </svg>
         </div>
         <div className="inner flex justify-center items-center h-[600px]">
           <div className="t-image w-[500px] h-[600px] after:content-[''] after:absolute after:w-[200px] after:h-[200px] after:background-[#3f56da] after:left-[140px] after:top-0 after:rounded-full">
-            <ul className="flex absolute border h-[460px] w-[460px]">
+            <ul className="flex absolute border h-[460px] w-[460px] shadow-[0px_0px_40px_rgba(0,0,10,0.25)]">
               {albums &&
                 albums.map((item, index) => {
                   return (
@@ -58,28 +58,30 @@ function Slider() {
                 })}
             </ul>
           </div>
+          <div className="t-content w-[500px] h-[400px] flex items-center">
+            <ul className="absolute overflow-hidden w-[500px] h-[400px]">
+              {albums &&
+                albums.map((item, index) => {
+                  return (
+                    <li
+                      key={index}
+                      className={`${
+                        activeAlbum === item.title ? "display" : "hidden"
+                      } w-[500px] h-[400px] flex items-center absolute`}
+                    >
+                      <div className="content-inner ">
+                        <p className="quote items-center justify-center">
+                          {item.title}
+                        </p>
+                        <p className="quote font-bold">{item.name}</p>
+                      </div>
+                    </li>
+                  );
+                })}
+            </ul>
+          </div>
         </div>
-        <div className="t-content">
-          <ul>
-            {albums &&
-              albums.map((item, index) => {
-                return (
-                  <li
-                    key={index}
-                    className={`${
-                      activeAlbum === item.title ? "display" : "hidden"
-                    }`}
-                  >
-                    <div className="content-inner">
-                      <p className="quote">{item.title}</p>
-                      <p className="quote font-bold">{item.name}</p>
-                    </div>
-                  </li>
-                );
-              })}
-          </ul>
-        </div>
-        <div className="arrows-right absolute top-0 bottom-0 flex justify-center items-center right-0 cursor-pointer">
+        <div className="arrows-right absolute top-0 bottom-0 flex justify-center items-center right-0 cursor-pointer hover:shadow-[0px_0px_30px_rgba(0,0,80,0.05)] rounded-[8px] w-[100px] duration-300 ease-in-out">
           <svg className="w-8 h-8">
             <ArrowRightIcon />
           </svg>
